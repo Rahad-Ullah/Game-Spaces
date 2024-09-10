64383b21b581601894be6b2727d8c91a945b4008
Rahad-Ullah <myemail.abc20@gmail.com>
2024-09-10 22:38:25 +0600

    Landing page has been added

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-medium text-center my-4">Sport facility booking platform</h1>
+    </div>
+  );
+};
+
+export default Home;
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,11 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../components/layout/MainLayout";
+import Home from "../pages/Home/Home";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <MainLayout />,
-    },
-  ]);
-  
-  export default router;
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+export default router;
