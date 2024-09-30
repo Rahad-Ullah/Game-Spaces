ae6eb5d585715b531cb918f60a1877019ec2109b
Rahad-Ullah <myemail.abc20@gmail.com>
2024-09-30 18:25:23 +0600

    Scroll to top button added

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,26 +1,13 @@
 import Navbar from "../shared/Navbar";
 import { Outlet } from "react-router-dom";
+import ScrollToTop from "../shared/ScrollToTop";
 
 const MainLayout = () => {
-  // Alert before reloading page
-  // useEffect(() => {
-  //   const handleBeforeUnload = (event: BeforeUnloadEvent) => {
-  //     const message =
-  //       "Are you sure you want to reload this page? Cart data might be lost.";
-  //     event.preventDefault();
-  //     return message;
-  //   };
-
-  //   window.addEventListener("beforeunload", handleBeforeUnload);
-
-  //   return () => {
-  //     window.removeEventListener("beforeunload", handleBeforeUnload);
-  //   };
-  // }, []);
   return (
     <div>
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <ScrollToTop />
     </div>
   );
 };
